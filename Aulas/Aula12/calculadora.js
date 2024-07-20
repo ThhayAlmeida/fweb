4b3abb2fa27cadbcc39c8033699915d6630dd354
ThhayAlmeida <thaynaradealmeida@gmail.com>
2024-07-19 22:02:16 -0300

    Add files via upload

---
+++ Aulas/Aula12/calculadora.js
@@ -0,0 +1,30 @@
+let textValue = "";
+
+function addNumber(addNumber){
+
+    updateTex()
+}
+
+function addOperator(Operador){
+
+    updateTex()
+}
+
+function addDecimal(){
+
+    updateTex()
+}
+
+function clearTex() {
+
+    updateTex()
+}
+
+function updateTex() {
+    document.querySelector /*continua*/
+}
+
+function calculate() {
+
+    updateTex()
+}
