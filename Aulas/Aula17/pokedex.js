ddf2541ee7f03c34f1e3064431c9634760ef2efa
ThhayAlmeida <thaynaradealmeida@gmail.com>
2024-08-02 21:55:28 -0300

    Add files via upload

--- Aulas/Aula17/pokedex.js
+++ Aulas/Aula17/pokedex.js
@@ -6,9 +6,12 @@ const botao1 = document.querySelector('#btn1');
 const botao2 = document.querySelector('#btn2');
 const pesoPokemon = document.querySelector('#pesoPokemon');
 const alturaPokemon = document.querySelector('#alturaPokemon');
-const tipoPokemon = document.querySelector('#tipoPokemon')
-const habilidadePokemon = document.querySelector('#habilidadePokemon')
+const form = document.querySelector('.form-busca');
+const inputName = document.querySelector('#inputName');
+const tipo1 = document.querySelector('#tipo1');
+const tipo2 = document.querySelector('#tipo2');
 
+let idPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -18,21 +21,40 @@ const fetchPokemon = async (pokemon) => {
 
 const showPokemon = async (pokemon) => {
     const dataPokemon = await fetchPokemon(pokemon);
+    idPokemon = dataPokemon.id;
     imgchooseyou.src = dataPokemon.sprites.front_default;
     nomePokemon.innerHTML = dataPokemon.name;
     numeroPokemon.innerHTML = dataPokemon.id;
     pesoPokemon.innerHTML = dataPokemon.weight;
     alturaPokemon.innerHTML = dataPokemon.height;
-    tipoPokemon.innerHTML = dataPokemon.types[0].type.name;
     audioPokemon.src = dataPokemon.cries.latest;
+    tipo1.innerHTML = dataPokemon.types[0].type.name;
+    if (dataPokemon.types.length > 1) {
+        tipo2.innerHTML = dataPokemon.types[1].type.name;
+    } else
+        tipo2.innerHTML = '';
 }
 
 botao1.addEventListener("click", () => {
-    showPokemon('2');
+    if (idPokemon > 1) {
+        idPokemon -= 1;
+        showPokemon(idPokemon);
+    }
+
 })
 
 botao2.addEventListener("click", () => {
-    showPokemon('25');
+    idPokemon += 1;
+    showPokemon(idPokemon);
 })
 
-showPokemon('2')
+inputName.addEventListener("input", () => {
+    idPokemon = toString(inputName.value);
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    showPokemon(inputName.value.toLowerCase());
+})
+
+showPokemon(idPokemon);
